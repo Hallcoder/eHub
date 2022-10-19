6be9c31b6d41103e5d1c217b3de8332eb77b11cd
Hallcoder <hallcoder25@gmail.com>
2022-10-19 22:02:51 +0200

    order popup

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
-function OrderPopup() {
-    return ( 
-        <div>
-            <h1>Order Details</h1>
-            
-        </div>
-     );
+import React, { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { headerClass } from '../constants';
+import Counter from './counter';
+function OrderPopup({image,desc,price,name,close}) {
+    let [count,setCount] = useState(0);
+    let [buy,setBuyStatus] = useState(true);
+    const changeqty = (action) => {
+        if(action == 'inc'){
+            setCount(++count);
+        }else{
+           setCount(--count);
+        }
+    };
+
+    const changeStatus = () => {
+        setBuyStatus(!buy);
+    }
+        return ( 
+            <div className="  min-h-fit min-w-fit w-9/12 sm:w-7/12 sm:ml-40  border-blue-500 p-1 rounded-md bg-white  border ">
+              <div className='flex justify-between '>
+                {buy && <h1 className={headerClass}>Order details</h1>}
+                {!buy && <h1 className={headerClass}>Payment Details</h1>}
+                <MdClose className='m-4 border-2 p-1 text-4xl rounded-full' onClick={() => close()}/>
+              </div>
+            {buy && <div>
+                <div className='w-full flex items-center flex-col p-2'>
+                    <img src={image} alt="Product"></img>
+                     <h1 className={headerClass}>{name}</h1>
+                     <h3>{count * price}</h3>
+                     <Counter qty={count} increase={() => changeqty('inc')} decrease={() => changeqty('decr')}/>
+                     <p>{desc}</p>
+                     <button className={'bg-blue-600 m-auto px-4 py-2 text-white rounded-sm'} onClick={changeStatus}>Buy</button>
+                </div>
+            </div>}
+            {!buy && <div>
+                
+                
+                </div>}
+            </div>
+         );
 }
 
 export default OrderPopup;
--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -1,44 +1,59 @@
-import * as actions from '../redux/actionTypes';
+import * as actions from "../redux/actionTypes";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Audio } from "react-loader-spinner";
-import {BsCartPlus} from 'react-icons/bs';
 import NavBar from "../components/navbar";
 import { AllQuery, buildImage, client, headerClass } from "../constants";
-import ProductCard from './../components/productCard';
+import ProductCard from "./../components/productCard";
 import { useDispatch } from "react-redux";
 import { createAction } from "../redux/actionCreator";
+import OrderPopup from "../components/Order";
 function ProductPage() {
   const buttonClass = "bg-blue-600 p-2 text-white m-1 font-bold";
   const params = useParams();
   const dispatch = useDispatch();
-  const [isInCart,setIsInCart] = useState(false);
+  const [isInCart, setIsInCart] = useState(false);
   const [product, setProduct] = useState({});
   const [products, setProducts] = useState([]);
+  const [popupVisible, setPopupVisible] = useState({
+    display: "none",
+  });
+
   useEffect(() => {
     const query = `*[_type == "product" && _id == "${params.id}" ]`;
-     client.fetch(query, {}).then(res => {
+    client.fetch(query, {}).then(res => {
       setProduct(res[0]);
       setIsInCart(product.isInCart);
     });
     client.fetch(AllQuery).then(res => {
-    console.log(res)
-    setProducts(res);
+      console.log(res);
+      setProducts(res);
+    });
+  }, []);
+  const handleAddToCart = () => {
+    dispatch(
+      createAction(actions.ADD_CART, {
+        product,
+      })
+    );
+    setIsInCart(!isInCart);
+  };
+  const handleShowPopup = () => {
+
+    if (popupVisible.display == "none") {
+      setPopupVisible({ display: "flex" });
+    } else {
+      setPopupVisible({ display: "none" });
     }
-    )
-  },[]);
-  const handleAddToCart =  () => {
-    dispatch(createAction(actions.ADD_CART,{
-      product,
-    }));
-    setIsInCart(!isInCart);
-  }
+  };
   const handleRemoveCart = () => {
-    dispatch(createAction(actions.REMOVE_CART,{
-        product
-    }));
+    dispatch(
+      createAction(actions.REMOVE_CART, {
+        product,
+      })
+    );
     setIsInCart(!isInCart);
-  }
+  };
   {
     return Object.values(product).length !== 0 ? (
       <div className="min-h-screen">
@@ -48,7 +63,11 @@ function ProductPage() {
  ] rounded-md m-auto"
         >
           <div>
-            <img src={buildImage(product.productImage.asset._ref).url()} alt="" className="ml-2 mt-2" />
+            <img
+              src={buildImage(product.productImage.asset._ref).url()}
+              alt=""
+              className="ml-2 mt-2"
+            />
           </div>
           <div className="w-full m-4 flex flex-col justify-start">
             <h1 className="font-bold m-2">
@@ -58,25 +77,73 @@ function ProductPage() {
             </h1>
             <h5 className="text-gray-600 m-4 font-bold">Details</h5>
             <div>
-             <h3 className="indent-4"> {Object.values(product).length !== 0
-                ? product.productDescription
-                : "Some Description"}</h3>
-                <h3 className="font-semibold indent-4">{'$'.concat(product.productPrice)}</h3>
+              <h3 className="indent-4">
+                {" "}
+                {Object.values(product).length !== 0
+                  ? product.productDescription
+                  : "Some Description"}
+              </h3>
+              <h3 className="font-semibold indent-4">
+                {"$".concat(product.productPrice)}
+              </h3>
             </div>
             <div className="m-auto w-10/12 flex justify-center">
-              {!isInCart ? <button className={buttonClass} onClick={() => handleAddToCart()}>Add Cart</button>:<button className={'bg-red-600 p-2 text-white m-1 font-bold'} onClick={() => handleRemoveCart()}>Remove</button>}
-              <button className={buttonClass}>Order</button>
+              {!isInCart ? (
+                <button
+                  className={buttonClass}
+                  onClick={() => handleAddToCart()}
+                >
+                  Add Cart
+                </button>
+              ) : (
+                <button
+                  className={"bg-red-600 p-2 text-white m-1 font-bold"}
+                  onClick={() => handleRemoveCart()}
+                >
+                  Remove
+                </button>
+              )}
+              <button className={buttonClass} onClick={handleShowPopup}>Order</button>
             </div>
           </div>
         </div>
         <div>
           <h1 className={headerClass}>Related Products</h1>
-         {
-          products.length !== 0 ? <div className="flex flex-wrap gap-4 sm:flex-row flex-col justify-center items-center">
-            {products.filter(p => p._id !== params.id).map(prod => {
-            return   <ProductCard key={prod._id} productName={prod.productName} image={buildImage(prod.productImage.asset._ref)} id={prod._id} manufacturer={prod .manufacturer}/>
-            })}
-          </div>:  <Audio
+          {products.length !== 0 ? (
+            <div className="flex flex-wrap gap-4 sm:flex-row flex-col justify-center items-center">
+              {products
+                .filter(p => p._id !== params.id)
+                .map(prod => {
+                  return (
+                    <ProductCard
+                      key={prod._id}
+                      productName={prod.productName}
+                      image={buildImage(prod.productImage.asset._ref)}
+                      id={prod._id}
+                      manufacturer={prod.manufacturer}
+                    />
+                  );
+                })}
+            </div>
+          ) : (
+            <Audio
+              height="80"
+              width="80"
+              radius="9"
+              color="blue"
+              ariaLabel="loading"
+              wrapperStyle
+              wrapperClass
+            />
+          )}
+        </div>
+        <div style={popupVisible} className="h-screen w-full  items-center justify-center fixed bg-black bg-opacity-60 top-0 p-28  shadow-xl shadow-black">
+          <OrderPopup close={handleShowPopup}  image={buildImage(product.productImage.asset._ref)} name={product.productName} price={product.productPrice} desc={product.productDescription}/>
+        </div>
+      </div>
+    ) : (
+      <div className="ml-[48vw] mt-[40vh] ">
+        <Audio
           height="80"
           width="80"
           radius="9"
@@ -85,20 +152,6 @@ function ProductPage() {
           wrapperStyle
           wrapperClass
         />
-         }
-        </div>
-      </div>
-    ) : (
-      <div className="ml-[48vw] mt-[40vh] ">
-        <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="blue"
-        ariaLabel="loading"
-        wrapperStyle
-        wrapperClass
-      />
       </div>
     );
   }
